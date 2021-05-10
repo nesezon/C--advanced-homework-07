492700797f8022911d5ad82bdddfda62eabdd7f8
nesezon <tilichenko@gmail.com>
2021-05-10 11:24:39 +0300

    Задание 2. InFileOutFile

---
+++ 2 InFileOutFile/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace InFileOutFile {
+  class Program {
+    static void Main(string[] args) {
+      string fileName = "test.txt";
+
+      try {
+        // если такой файл есть, удаляю
+        if (File.Exists(fileName)) File.Delete(fileName);
+
+        // Создаю новый файл и пишу в него
+        using (FileStream fs = File.Create(fileName)) {
+          using (StreamWriter sw = new StreamWriter(fs, Encoding.Unicode)) {
+            sw.WriteLine("Первая строка текста...");
+            sw.WriteLine("Вторая строка текста...");
+            sw.Write(sw.NewLine);
+            sw.WriteLine("Третья строка чисел:");
+            for (int i = 0; i < 10; i++) {
+              sw.Write($"{i} ");
+            }
+          }
+        }
+
+        // открываю файл на чтение и вывожу содержимое
+        using (StreamReader sr = File.OpenText(fileName)) {
+          string s = "";
+          while ((s = sr.ReadLine()) != null) {
+            Console.WriteLine(s);
+          }
+        }
+      } catch (Exception Ex) {
+        Console.WriteLine(Ex.ToString());
+      }
+
+      // Задержка.
+      Console.ReadKey();
+    }
+  }
+}
